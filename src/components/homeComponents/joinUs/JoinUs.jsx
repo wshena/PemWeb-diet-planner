25bb44e13321b7ffda5741792c5c07d5275632e1
poppywydy <adeliasekar7@gmail.com>
2022-12-21 20:04:57 +0700

    mengubah ke bahasa indonesia

--- src/components/homeComponents/joinUs/JoinUs.jsx
+++ src/components/homeComponents/joinUs/JoinUs.jsx
@@ -7,7 +7,7 @@ function JoinUs() {
 			<div className="joinUs-text">
 				<h3>Tertarik untuk bergabung?</h3>
 				<h1>Dapatkan program diet anda dan mulailah berpetualang!</h1>
-				<a href="/register"> <button className='btn btn-register'>Register</button> </a>
+				<a href="/register"> <button className='btn btn-register'>Daftar</button> </a>
 			</div>
 			<div className="joinUs-image" style={{
 				backgroundImage : `url(${bg})`,
